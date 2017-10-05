d10a1c2763fadc9d3da08aa98e62d9a4c78d7a1b
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2017-10-05 19:33:35 +0300

    No more accidental SQLite3, never

--- languk/languk/settings/base.py
+++ languk/languk/settings/base.py
@@ -99,8 +99,7 @@ WSGI_APPLICATION = 'languk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
